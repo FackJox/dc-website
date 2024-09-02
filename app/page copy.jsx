6f78d8efb8575884ed9f35b84b6aad6b9731c4e7
fackjox <jackfox@tuta.io>
2024-09-02 20:51:08 +0100

    first commit

--- app/page copy.jsx
+++ app/page copy.jsx
@@ -29,9 +29,9 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
         {/* jumbo */}
         <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>DrumCult Presents</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Initiation</h1>
-          <p className='mb-8 text-2xl leading-normal'>A brand new experience for morning joggers and dogwalkers</p>
+          <p className='w-full uppercase'>Next + React Three Fiber</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
+          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
         </div>
 
         <div className='w-full text-center md:w-3/5'>
@@ -47,10 +47,8 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
         {/* first row */}
         <div className='relative h-48 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events</h2>
-          <p className='mb-8 text-gray-600'>INITIATION - 28th Sept 24</p>
-          <p className='mb-8 text-gray-600'>jcak fcuks, Matt Scratch, Pscilcyben Laden, </p>
-
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
+          <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
         </div>
         <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
           <View orbit className='relative h-full  sm:h-48 sm:w-full'>
@@ -70,9 +68,12 @@ export default function Page() {
           </View>
         </div>
         <div className='w-full p-6 sm:w-1/2'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Socials</h2>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h2>
           <p className='mb-8 text-gray-600'>
-           TikTok Logo, FB Logo, Insta Logo - look at carousels
+            3D Divs are renderer through the View component. It uses gl.scissor to cut the viewport into segments. You
+            tie a view to a tracking div which then controls the position and bounds of the viewport. This allows you to
+            have multiple views with a single, performant canvas. These views will follow their tracking elements,
+            scroll along, resize, etc.
           </p>
         </div>
       </div>
